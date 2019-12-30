241c31be4a28170340f8cbd38044a260e6dbc1b1
Mohini Thakur <59048563+Mohinithakur@users.noreply.github.com>
2019-12-30 13:37:32 +0530

    to create your own module

---
+++ my_module.py
@@ -0,0 +1,18 @@
+"""
+It will only run at the location it is created.since it is created by me,so it is
+loctaion dependent.
+"""
+
+
+import time
+
+def clear():
+  print("\n"*50)
+
+def sum(*x):
+  a=0
+  for i in x:
+    a=a+id
+    return a
+def notd():
+  print ("hello")
